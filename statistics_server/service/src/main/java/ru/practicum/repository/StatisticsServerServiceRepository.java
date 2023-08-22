c6a485ba75cf9b3f4b34048714545c2637dd1da0
Vladislav Surkov <vladucha09@inbox.ru>
2023-08-22 04:34:48 +0300

    feat : application.properties        docker-compose.yml        Dockerfile        pom.xml        pom.xml        statistics_server        dto        service

--- statistics_server/service/src/main/java/ru/practicum/repository/StatisticsServerServiceRepository.java
+++ statistics_server/service/src/main/java/ru/practicum/repository/StatisticsServerServiceRepository.java
@@ -1,12 +1,7 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import ru.practicum.ViewStatsDto;
 import ru.practicum.model.Endpoint;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface StatisticsServerServiceRepository extends JpaRepository<Endpoint, Long> {
 
