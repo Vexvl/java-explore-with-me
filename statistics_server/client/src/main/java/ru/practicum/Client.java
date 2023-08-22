49a2b42cbf8e0527f10208c5bc6faed705093d84
Vladislav Surkov <vladucha09@inbox.ru>
2023-08-22 05:51:33 +0300

    feat : application.properties        docker-compose.yml        Dockerfile        pom.xml        pom.xml        statistics_server        dto        service

--- statistics_server/client/src/main/java/ru/practicum/Client.java
+++ statistics_server/client/src/main/java/ru/practicum/Client.java
@@ -1,23 +1,37 @@
 package ru.practicum;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+@Service
 public class Client {
-    protected final RestTemplate rest = new RestTemplate();
+
     private final String path = "http://localhost:9090";
+    private final RestTemplate restTemplate;
+
+    public Client(@Value("${statistics_service.url}") String serverUrl) {
+        this.restTemplate = new RestTemplateBuilder()
+                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+    }
 
     public void saveEndpoint(EndpointDto endpointDto) {
-        rest.postForLocation(path + "/hit", endpointDto);
+        restTemplate.postForLocation(path + "/hit", endpointDto);
     }
 
     public List<ViewStatsDto> getViewStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        ResponseEntity<ViewStatsDto[]> responseResult = rest.getForEntity(path + "/stats" + "?start=" + start + "&end=" + end +
+        ResponseEntity<ViewStatsDto[]> responseResult = restTemplate.getForEntity(path + "/stats" + "?start=" + start + "&end=" + end +
                 "&uris=" + uris + "&unique=" + unique, ViewStatsDto[].class);
         return Arrays.asList(Objects.requireNonNull(responseResult.getBody()));
     }
