accef2ffdd7597d150bba4fafa57c42a4ac86add
Vladislav Surkov <vladucha09@inbox.ru>
2023-09-03 14:02:35 +0300

    style changes: schema colums name events, users changed to event_id, user_id

--- ewm/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
+++ ewm/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.practicum.request.dto.ParticipationRequestDto;
 import ru.practicum.request.model.EventRequestStatusUpdateRequest;
 import ru.practicum.request.model.EventRequestStatusUpdateResult;
